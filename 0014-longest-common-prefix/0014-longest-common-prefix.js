66ca539e00e851ef8b43771c8f1b4c86cb3474ab
YOUSAF SHIBIN M P <yousafmpshibin@gmail.com>
2025-11-12 00:01:11 +0530

    Time: 0 ms (100%), Space: 54.7 MB (59.77%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+};
