682aa34b7d1dc362c1774e299e0abc75733e08a3
YOUSAF SHIBIN M P <yousafmpshibin@gmail.com>
2025-11-14 23:54:50 +0530

    Time: 1 ms (76.17%), Space: 59.8 MB (57.26%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let j = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    while (j < nums.length) {
+        nums[j] = 0;
+        j++;
+    }
+};
