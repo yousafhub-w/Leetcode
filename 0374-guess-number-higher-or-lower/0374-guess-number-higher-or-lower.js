883fde46f33f3508c401daf4a275a5ca9c201797
YOUSAF SHIBIN M P <yousafmpshibin@gmail.com>
2025-11-16 23:08:17 +0530

    Time: 45 ms (59.48%), Space: 52.1 MB (99.24%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -12,16 +12,14 @@
  * @return {number}
  */
 var guessNumber = function(n) {
-    let low = 1;
-    let high = n;
+    let low = 1, high = n;
 
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2);
+    while (true) {
+        let mid = (low + high) >>> 1;
 
         let res = guess(mid);
-
         if (res === 0) return mid;
-        else if (res === -1) high = mid - 1;
+        if (res < 0) high = mid - 1;
         else low = mid + 1;
     }
 };
