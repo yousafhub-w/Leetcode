3292b74689751a4a237b84b5c4b7308af714e1bf
YOUSAF SHIBIN M P <yousafmpshibin@gmail.com>
2025-11-20 01:38:38 +0530

    Time: 11 ms (68.68%), Space: 63 MB (32.5%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    const stack = [];
+
+    for (let char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();     
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
