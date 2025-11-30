c6893f00735565a38a128eeff67a0ee9e6d52624
YOUSAF SHIBIN M P <yousafmpshibin@gmail.com>
2025-11-30 17:09:44 +0530

    Time: 1 ms (87.24%), Space: 58.5 MB (10.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,13 +1,13 @@
 var twoSum = function(nums, target) {
-  const map = new Map(); // store number -> index
+  const map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     let complement = target - nums[i];
 
     if (map.has(complement)) {
-      return [map.get(complement), i]; // found the pair
+      return [map.get(complement), i];
     }
 
-    map.set(nums[i], i); // store current number and index
+    map.set(nums[i], i);
   }
 };
